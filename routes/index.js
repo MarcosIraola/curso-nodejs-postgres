c97bd37bbb4a962cd40c30bff942ec5990caab5f
MarcosIraola <iraolamarcos@gmail.com>
2022-09-11 23:59:49 -0300

    match model

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
 const usersRouter = require('./users.router');
-const matchsRouter = require('./matchs.router');
+const matchesRouter = require('./matches.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/users', usersRouter);
-  router.use('/matchs', matchsRouter);
+  router.use('/matches', matchesRouter);
 }
 
 module.exports = routerApi;
